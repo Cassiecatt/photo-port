09b47c47da3ab1d92d45631ef89c037965c399a2
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-08 18:46:03 -0700

    updated test file

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -2,32 +2,43 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
+import Contact from '../../Contact';
 
 const categories = [
-  { name: 'portraits', description: 'Portraits of people in my life' }
-]
+  { name: "portraits", description: "Portraits of people in my life" },
+];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
 describe("Nav component", () => {
   // baseline test
-  it('renders', () => {
-    render(<Nav
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
-    />);
+  it("renders", () => {
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
   });
 
   // snapshot test
   it("matches snapshot", () => {
-    const { asFragment } = render(<Nav
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
-    />);
+    const { asFragment } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
     // assert value comparison
     expect(asFragment()).toMatchSnapshot();
   });
@@ -36,11 +47,15 @@ describe("Nav component", () => {
 //emoji test
 describe("emoji is visible", () => {
   it("inserts emoji into the h2", () => {
-    const { getByLabelText } = render(<Nav
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
-    />);
+    const { getByLabelText } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
 
     expect(getByLabelText("camera")).toHaveTextContent("📸");
   });
@@ -49,11 +64,15 @@ describe("emoji is visible", () => {
 //link test
 describe("links are visible", () => {
   it("inserts text into the links", () => {
-    const { getByTestId } = render(<Nav
-      categories={categories}
-      setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}
-    />);
+    const { getByTestId } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
     expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
     expect(getByTestId("about")).toHaveTextContent("About me");
   });
